f322a19e155bfacb7dc146186cbcd4a02ccf0889
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-03-26 21:53:25 -0300

    built out tests, output.js, prompt.js. issues runnig app

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-import { Circle, Square, Triangle } from "./lib/shapes.js";
+import inquirer from "inquirer";
+const questions = require("./lib/prompt.js");
+const generateShape = require("./lib/shapes.js");
+const writeToFile = require("./lib/output.js");
 
-const circle = new Circle("purple");
+function start() {
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      let svg = `
+<svg
+version="1.1"
+width="300"
+height="200"
+xmlns="http://www.w3.org/2000/svg"
+>
+  ${generateShape(answers.shape, answers.shapeColor)}
 
-console.log(circle);
+  <text x="151" y="120" font-size="60" text-anchor="middle" fill="${
+    answers.textColor
+  }">
+    ${answers.text}
+  </text>
+</svg>
+      `;
+      writeToFile(svg);
+    })
+    .catch((error) => {
+      if (error.isTyError) {
+        // prompt couldn't be rendered in the current environment.
+      } else {
+        console.log(error);
+      }
+    });
+}
+
+start();
